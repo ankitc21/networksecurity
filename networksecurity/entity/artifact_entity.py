ca8238db9caae9e4fcda549d19eb1f72d9d1fec2
ankitc21 <ankit004c@gmail.com>
2025-07-17 00:43:57 +0530

    trainer and mlflow

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -19,7 +19,7 @@ class DataTransformationArtifact:
     transformed_object_file_path: str
     transformed_train_file_path: str
     transformed_test_file_path: str
-'''
+
 @dataclass
 class ClassificationMetricArtifact:
     f1_score: float
@@ -30,4 +30,4 @@ class ClassificationMetricArtifact:
 class ModelTrainerArtifact:
     trained_model_file_path: str
     train_metric_artifact: ClassificationMetricArtifact
-    test_metric_artifact: ClassificationMetricArtifact'''
+    test_metric_artifact: ClassificationMetricArtifact
